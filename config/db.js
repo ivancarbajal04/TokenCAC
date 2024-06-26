f9045f9b26d21ccd92cb5a969ee7a532d3270f44
Aprende Programando <aprende.programando@bue.edu.ar>
2024-06-26 14:40:19 -0300

    Actualizaciones para deploy en Railway

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password: '',
-    database:'users'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'users'
 })
 
 connection.connect(err => {
