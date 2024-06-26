6bc98f68fbd4ee173c9b17a7de56ce1a58b529ed
Aprende Programando <aprende.programando@bue.edu.ar>
2024-06-26 15:15:39 -0300

    sacado de carpeta front, de --watch y eliminado de carpeta models e importaciones

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
-const users = require('../models/userModel')
 const config = require('../config/config')
 const connection = require('../config/db')
 
